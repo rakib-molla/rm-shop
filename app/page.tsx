2f54f4ddf2f2ec3b8f444b6b1ba55759b9b81376
unknown <rakibmail1997@gmail.com>
2025-11-06 01:41:35 +0600

    home page section add

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      Home
+    <div>
+      <section>Home Page</section>
+      <div>hero section </div>
     </div>
   );
 }
